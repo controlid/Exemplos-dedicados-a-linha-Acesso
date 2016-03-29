835caec20b39fb3a68cdc08fa7e840cc50ef3e34
Fabio Ferreira <fabio.ferreira@controlid.com.br>
2016-03-29 17:50:39 -0300

    Áreas e Portais

--- test-CS/BaseTest.cs
+++ test-CS/BaseTest.cs
@@ -13,7 +13,7 @@ namespace UnitTestAcesso
 {
     public partial class BaseTest
     {
-        public static string URL = "http://192.168.2.120/";
+        public static string URL = "http://192.168.2.121/";
         public static string Login = "admin";
         public static string Password = "admin";
     }
---
+++ test-CS/sdkTest_Areas.cs
@@ -0,0 +1,30 @@
+﻿using ControlID;
+using ControlID.iDAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestAcesso
+{
+    public partial class sdkTest
+    {
+        [TestMethod, TestCategory("sdk generic")]
+        public void sdk_Areas_CRUD()
+        {
+            long areaFora = eqpt.LoadOrAdd<Areas>("API Fora");
+            long areaDentro = eqpt.LoadOrAdd<Areas>("API Dentro");
+            Console.WriteLine("Areas de Dentro: " + areaDentro + " e Fora: " + areaFora);
+
+            long Entrando = eqpt.LoadOrAdd<Portals>(new Portals() { name = "API Entrando", area_from_id = areaFora, area_to_id = areaDentro });
+            long Saindo = eqpt.LoadOrAdd<Portals>(new Portals() { name = "API Saindo", area_from_id = areaDentro, area_to_id = areaFora });
+            Console.WriteLine("Portais Entrando: " + Entrando + " e Saindo: " + Saindo);
+
+
+        }
+    }
+}
