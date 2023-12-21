ee6f2567a5f897f291f7cfaf3391646196ee06e6
lucabasilio <luca.basilio@controlid.com.br>
2023-12-21 15:50:42 -0300

    Suporte à iDBox V2

--- sdkAcesso/iDAccess/Device_Utils.cs
+++ sdkAcesso/iDAccess/Device_Utils.cs
@@ -114,14 +114,17 @@ namespace ControliD.iDAccess
         //[Description("iDAccess Light Bio ASK+MIFARE")]    V,
 
 // iDUHF
-        [Description("iDUHF")]                             _0N01,
-        [Description("iDUHF Lite")]                        _0N02,
+        [Description("iDUHF")]                              _0N01,
+        [Description("iDUHF Lite")]                         _0N02,
 
 // iDFace
         [Description("iDFace Legacy1")]                     _0M01,
         [Description("iDFace Legacy2")]                     _0M02,
         [Description("iDFace")]                             _0M03,
         [Description("iDFace HID")]                         _0M04,
+
+// iDBox V2
+        [Description("iDBox V2")]                           _0L01,
 
         [Description("(não identificado)")]                 none
     }
